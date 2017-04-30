6e0b4cf608f3df2cb0737ec10cd2ec7a6fe6f736
Laurent Curau <laurentcurau@gmail.com>
2017-04-30 17:39:05 +0100

    Set up RSpec

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -99,6 +99,3 @@ class Game
   end
 
 end
-
-game = Game.new
-game.start_game
---
+++ spec/tic_tac_toe/game_spec.rb
@@ -0,0 +1,7 @@
+require "tic_tac_toe/game"
+
+describe Game do
+  it "says true is true" do
+    expect(true).to eql true
+  end
+end
