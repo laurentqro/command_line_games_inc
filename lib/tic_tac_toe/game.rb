7865fd22e21e1b52a48fa1ebbc7a7b2cccc9f8de
Laurent Curau <laurentcurau@gmail.com>
2017-04-30 21:27:07 +0100

    Add test coverage to existing code

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -1,29 +1,43 @@
 class Game
-  def initialize
-    @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+  attr_reader :board
+
+  def initialize(board: ["0", "1", "2", "3", "4", "5", "6", "7", "8"])
+    @board = board
     @com = "X" # the computer's marker
     @hum = "O" # the user's marker
   end
 
   def start_game
-    # start by printing the board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
-    puts "Enter [0-8]:"
-    # loop through until the game was won or tied
+    print_board
+    prompt_player_input
+
     until game_is_over(@board) || tie(@board)
       get_human_spot
       if !game_is_over(@board) && !tie(@board)
-        eval_board
+        get_computer_spot
       end
-      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+      print_board
     end
+    puts notify_game_over
+  end
+
+  def print_board
+    # start by printing the board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+  end
+
+  def prompt_player_input
+    puts "Enter [0-8]:"
+  end
+
+  def notify_game_over
     puts "Game over"
   end
 
   def get_human_spot
     spot = nil
     until spot
-      spot = gets.chomp.to_i
+      spot = get_input
       if @board[spot] != "X" && @board[spot] != "O"
         @board[spot] = @hum
       else
@@ -32,14 +46,14 @@ class Game
     end
   end
 
-  def eval_board
+  def get_computer_spot
     spot = nil
     until spot
       if @board[4] == "4"
         spot = 4
         @board[spot] = @com
       else
-        spot = get_best_move(@board, @com)
+        spot = get_best_move(@board)
         if @board[spot] != "X" && @board[spot] != "O"
           @board[spot] = @com
         else
@@ -49,21 +63,25 @@ class Game
     end
   end
 
-  def get_best_move(board, next_player, depth = 0, best_score = {})
+  def get_best_move(board, depth = 0, best_score = {})
     available_spaces = []
     best_move = nil
-    board.each do |s|
-      if s != "X" && s != "O"
-        available_spaces << s
+
+    board.each do |space|
+      if space != "X" && space != "O"
+        available_spaces << space
       end
     end
+
     available_spaces.each do |as|
       board[as.to_i] = @com
+      # if can win with next move, play that
       if game_is_over(board)
         best_move = as.to_i
         board[as.to_i] = as
         return best_move
       else
+        # block human winning with next move
         board[as.to_i] = @hum
         if game_is_over(board)
           best_move = as.to_i
@@ -74,16 +92,12 @@ class Game
         end
       end
     end
-    if best_move
-      return best_move
-    else
-      n = rand(0..available_spaces.count)
-      return available_spaces[n].to_i
-    end
+
+    n = rand(0..available_spaces.count)
+    return available_spaces[n].to_i
   end
 
   def game_is_over(b)
-
     [b[0], b[1], b[2]].uniq.length == 1 ||
     [b[3], b[4], b[5]].uniq.length == 1 ||
     [b[6], b[7], b[8]].uniq.length == 1 ||
@@ -98,4 +112,9 @@ class Game
     b.all? { |s| s == "X" || s == "O" }
   end
 
+  private
+
+  def get_input
+    gets.chomp.to_i
+  end
 end
--- spec/tic_tac_toe/game_spec.rb
+++ spec/tic_tac_toe/game_spec.rb
@@ -1,7 +1,68 @@
 require "tic_tac_toe/game"
 
 describe Game do
-  it "says true is true" do
-    expect(true).to eql true
+  describe "#print_board" do
+    it "prints the board" do
+      expected_output =  " 0 | 1 | 2 \n===+===+===\n 3 | 4 | 5 \n===+===+===\n 6 | 7 | 8 \n"
+
+      expect { Game.new.print_board }.to output(expected_output).to_stdout
+    end
+  end
+
+  describe "#prompt_player_input" do
+    it "tells the player when the game is over" do
+      expect { Game.new.prompt_player_input }.to output("Enter [0-8]:\n").to_stdout
+    end
+  end
+
+  describe "#notify_game_over" do
+    it "tells the player when the game is over" do
+      expect { Game.new.notify_game_over }.to output("Game over\n").to_stdout
+    end
+  end
+
+  describe "#get_human_spot" do
+    it "gets human player's play" do
+      game = Game.new
+      allow(game).to receive(:get_input) { 8 }
+      game.get_human_spot
+
+      expect(game.board).to eql ["0", "1", "2", "3", "4", "5", "6", "7", "O"]
+    end
+  end
+
+  describe "#get_computer_spot" do
+    it "plays 4 if position is available" do
+      game = Game.new
+      game.get_computer_spot
+
+      expect(game.board).to eql ["0", "1", "2", "3", "X", "5", "6", "7", "8"]
+    end
+
+    it "plays its best move if position 4 is not available" do
+      board = ["0", "1", "2", "3", "O", "5", "6", "7", "8"]
+      game = Game.new(board: board)
+      allow(game).to receive(:get_best_move).with(board).and_return(1)
+
+      game.get_computer_spot
+
+      expect(game.board).to eql ["0", "X", "2", "3", "O", "5", "6", "7", "8"]
+    end
+  end
+
+  describe "#get_best_move" do
+    it "plays for the win" do
+      board = ["X", "X", "2", "O", "O", "5", "6", "7", "8"]
+      game = Game.new(board: board)
+
+      expect(game.get_best_move(board)).to eql 2
+    end
+
+    it "blocks opponent from winning on his next move" do
+      board = ["X", "O", "X", "O", "O", "5", "6", "7", "8"]
+      game = Game.new(board: board)
+
+      expect(game.get_best_move(board)).to eql 5
+    end
   end
 end
