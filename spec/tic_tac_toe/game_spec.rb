f24cb0fe9d613228b0984d7d0e90df2763db657b
Laurent Curau <laurentcurau@gmail.com>
2017-11-26 18:15:40 +0000

    Test: game is over when there is a tie

--- spec/tic_tac_toe/game_spec.rb
+++ spec/tic_tac_toe/game_spec.rb
@@ -14,9 +14,29 @@ describe Game do
       display: display
     )
 
-    board = Board.new(grid: ["X", "O", "O",
-                             "4", "X", "6",
-                             "7", "8", "X"])
+    grid = ["X", "X", "X",
+            "3", "4", "5",
+            "6", "7", "8"]
+
+    board = Board.new(grid: grid)
+
+    game = Game.new(board: board, config: config)
+
+    expect(game).to be_over
+  end
+
+  it "is over when there is a tie" do
+    config = Config.new(
+      player_x: TestPlayer.new(mark: "X"),
+      player_o: TestPlayer.new(mark: "O"),
+      display: display
+    )
+
+    grid = ["O", "X", "O",
+            "X", "O", "X",
+            "O", "X", "O"]
+
+    board = Board.new(grid: grid)
 
     game = Game.new(board: board, config: config)
 
